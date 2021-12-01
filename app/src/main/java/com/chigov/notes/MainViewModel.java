330f54038bd68d28271e3f34824e98ccb54be0e7
Max2 <chigov@gmail.com>
2021-12-01 22:28:37 +0200

    11

--- app/src/main/java/com/chigov/notes/MainViewModel.java
+++ app/src/main/java/com/chigov/notes/MainViewModel.java
@@ -1,4 +1,24 @@
 package com.chigov.notes;
 
-public class MainViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+    //получить БД и присвоить ей значение
+    private NotesDatabase database;
+    //создаем обект, в которм будем хранить все заметки
+    private LiveData<List<Note>> notes;
+    //и присвоим значение в конструкторе
+
+    //constructor
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        database = NotesDatabase.getInstance(getApplication());
+        notes =
+    }
 }
